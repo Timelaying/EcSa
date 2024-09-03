2764f5c73d85cadb6aa6b31027ce4191e4938984
Timelaying <makanjuola.timi@gmail.com>
2024-09-03 20:33:59 +0100

    new product ui buld

--- ecsa/src/app/admin/products/page.tsx
+++ ecsa/src/app/admin/products/page.tsx
@@ -1,13 +1,40 @@
 import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/PageHeader";
+import Link from "next/link";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function AdminProductsPage() {
     return (  
-    <div>
+    <div className="flex justify-between items-center gap-4">
         <PageHeader>Products</PageHeader>
-        <Button></Button>
-
+        <Button asChild>
+            <Link href="/admin/products/new">Add Product</Link>
+        </Button>
+        <ProductsTable/>
     </div>
     )
       
+}
+
+function ProductsTable(){
+    return(
+    <Table>
+        <TableHeader>
+            <TableRow>
+                <TableHead className="w-0">
+                    <span className="sr-only">Avaliable For Purchase</span>
+                </TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead>Orders</TableHead>
+                <TableHead className="w-0">
+                    <span className="sr-only">ACtions</span>
+                </TableHead>
+            </TableRow>
+        </TableHeader>
+
+        <TableBody></TableBody>
+        
+    </Table>
+    )
 }
