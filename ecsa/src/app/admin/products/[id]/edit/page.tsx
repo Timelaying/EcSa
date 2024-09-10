c005f8601ea24a990747058513e5f9ca88975b63
Timelaying <makanjuola.timi@gmail.com>
2024-09-10 13:41:03 +0100

    update product

--- ecsa/src/app/admin/products/[id]/edit/page.tsx
+++ ecsa/src/app/admin/products/[id]/edit/page.tsx
@@ -2,13 +2,13 @@ import db from "@/db/db";
 import { PageHeader } from "../../../_components/PageHeader";
 import { ProductForm } from "../../_components/ProductForm";
 
-export default function EditProductPage({params: {id},}: {params: {id: string} } }) {
-const product = await db.product.findUnique{{where: {id}}}    
+export default async function EditProductPage({params: {id},}: {params: {id: string} } ) {
+const product = await db.product.findUnique({ where: {id}})    
 return(
     // empty parent -> 
     <> 
     <PageHeader>Edit Product</PageHeader>
-    <ProductForm/>
+    <ProductForm product= {product}/>
     </> 
 )
 }
