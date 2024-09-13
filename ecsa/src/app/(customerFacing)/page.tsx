652dbfe81898eec3ffa41b6c6f89dd3185d58602
Timelaying <makanjuola.timi@gmail.com>
2024-09-13 19:47:06 +0100

    customer button begin

--- ecsa/src/app/(customerFacing)/page.tsx
+++ ecsa/src/app/(customerFacing)/page.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button"
 import db from "@/db/db"
 import { Product } from "@prisma/client"
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
 
 function getMostPopularProducts() {
    return  db.product.findMany({
@@ -19,9 +22,8 @@ function getMostPopularProducts() {
  
  export default function HomePage() { 
     return <main className="space-y-12">
-      <ProductGridSection productsFetcher={getMostPopularProducts}/>
-      <ProductGridSection productsFetcher={getNewestProducts}/>
-
+      <ProductGridSection title="Most Popular" productsFetcher={getMostPopularProducts}/>
+      <ProductGridSection title="Newest" productsFetcher={getNewestProducts}/>
     </main>
  }
 
@@ -29,11 +31,20 @@ function getMostPopularProducts() {
    title: string
    productsFetcher: () => Promise<Product[]>
  }
-
+ 
  function ProductGridSection({productsFetcher, title,}: ProductGridSectionProps) {
-   ;<div className="space-y-4">
+   return (<div className="space-y-4">
       <div className="flex- gap-4">
          <h2 className="text-3xl font-bold">{title}</h2>
+         <Button variant="outline" asChild>
+            <Link href="/products" className="space-x-2">
+            <span>View all</span>
+            <ArrowRight className="size-4" />
+            </Link>
+         </Button>
       </div>
-   </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+         
+      </div>
+   </div>)
  }
