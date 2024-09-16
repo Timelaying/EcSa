b42dfffede51f470c7fba2db0e4d6c8b872bb4b0
Timelaying <makanjuola.timi@gmail.com>
2024-09-16 20:25:56 +0100

    customer page product crd

--- ecsa/src/app/(customerFacing)/page.tsx
+++ ecsa/src/app/(customerFacing)/page.tsx
@@ -1,3 +1,4 @@
+import { ProductCard } from "@/components/ProductCard"
 import { Button } from "@/components/ui/button"
 import db from "@/db/db"
 import { Product } from "@prisma/client"
@@ -32,7 +33,7 @@ function getMostPopularProducts() {
    productsFetcher: () => Promise<Product[]>
  }
  
- function ProductGridSection({productsFetcher, title,}: ProductGridSectionProps) {
+async function ProductGridSection({productsFetcher, title,}: ProductGridSectionProps) {
    return (<div className="space-y-4">
       <div className="flex- gap-4">
          <h2 className="text-3xl font-bold">{title}</h2>
@@ -44,7 +45,9 @@ function getMostPopularProducts() {
          </Button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-         <ProductCard />
+         {(await productsFetcher()).map(product =>(
+            <ProductCard key={product.id} {...product} />
+         ))}
       </div>
    </div>)
  }
