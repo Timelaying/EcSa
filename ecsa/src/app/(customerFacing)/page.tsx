4c86329d753e3a6a6a5938fbf64b37d599ec9cb7
Timelaying <makanjuola.timi@gmail.com>
2024-09-16 20:19:41 +0100

    ProductCard

--- ecsa/src/app/(customerFacing)/page.tsx
+++ ecsa/src/app/(customerFacing)/page.tsx
@@ -44,7 +44,7 @@ function getMostPopularProducts() {
          </Button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-         
+         <ProductCard />
       </div>
    </div>)
  }
