bfd8ccf8f4b993c5dd43548f9cae38f76eb0c44b
Timelaying <makanjuola.timi@gmail.com>
2024-09-18 14:40:05 +0100

    payment_intent

--- ecsa/src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ ecsa/src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -2,11 +2,17 @@ import db from "@/db/db"
 import { notFound } from "next/navigation"
 import Stripe from "stripe"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function PurchasePage({params: { id },}: {params: { id: string}}){
     const product = await db.product.findUnique({where: { id }})
     if (product == null) return notFound()
     
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: product.priceInCents,
+        currency: "USD",
+        metadata: {productId: product.id}
+    })
+
     return <h1>Hp</h1>
 }
