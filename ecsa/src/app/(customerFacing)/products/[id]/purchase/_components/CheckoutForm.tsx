b3d563621899f56cd5ab0a47ad070c3acadae94d
Timelaying <makanjuola.timi@gmail.com>
2024-09-19 17:47:54 +0100

    cards stripes

--- ecsa/src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ ecsa/src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,13 +1,43 @@
-import { Elements } from "@stripe/react-stripe-js"
+"use client"
+
+import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
+import Image from "next/image"
 
 type CheckoutFormProps = {
-    product: {}
+    product: {
+        imagePath : string
+        name: string
+        priceInCents: number
+        description: string
+    }
     clientSecret: string
 }
 
-const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string)
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string)
 
 export function CheckoutForm({product, clientSecret }: CheckoutFormProps){
-    return <Elements
+    return(
+    <div className="max-w-5xl w-full mx-auto space-y-8">
+        <div className="flex gap-4 items-center">
+            <div className="aspect-video flex-shrink-0 w-1/3 relative">
+                <Image src={product.imagePath} fill alt={product.name} />
+            </div>
+            <div>
+                
+            </div>
+        </div>
+
+        <Elements options={{ clientSecret }} stripe={stripePromise}>
+         <Form />
+        </Elements>
+    </div>
+    )
+}
+
+function Form() {
+    const stripe = useStripe()
+    const elements = useElements()
+
+    return <PaymentElement />
 }
