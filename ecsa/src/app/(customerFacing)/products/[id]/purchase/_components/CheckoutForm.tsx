97725abc3836cbac13444129ab69e8331bdbe83c
Timelaying <makanjuola.timi@gmail.com>
2024-09-19 17:52:22 +0100

    text fro stripe

--- ecsa/src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ ecsa/src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { formatCurrency } from "@/lib/formatters"
 import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
 import Image from "next/image"
@@ -21,10 +22,14 @@ export function CheckoutForm({product, clientSecret }: CheckoutFormProps){
     <div className="max-w-5xl w-full mx-auto space-y-8">
         <div className="flex gap-4 items-center">
             <div className="aspect-video flex-shrink-0 w-1/3 relative">
-                <Image src={product.imagePath} fill alt={product.name} />
+                <Image src={product.imagePath} fill alt={product.name} className="object-cover" />
             </div>
             <div>
-                
+                <div className="text-lg">
+                    {formatCurrency(product.priceInCents / 100)}
+                </div>
+                <h1 className="text-2x1 font-bold">{product.name}</h1>
+                <div className="line-clamp-3 text-muted-foreground">{product.description}</div>
             </div>
         </div>
 
