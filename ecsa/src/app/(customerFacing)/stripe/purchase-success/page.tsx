dfc4b2423b8b38001164092a90c45f72955bb223
Timelaying <makanjuola.timi@gmail.com>
2024-09-23 21:14:37 +0100

    messages and checks for successful purchase

--- ecsa/src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ ecsa/src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,7 +1,25 @@
+import db from "@/db/db";
 import { formatCurrency } from "@/lib/formatters";
 import Image from "next/image";
+import { notFound } from "next/navigation";
+import Stripe from "stripe";
 
-export default function SuccessPage({searchParams, }: {searchParams: { payment_intent: string}}){
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+
+export default async function SuccessPage({searchParams, }: {searchParams: { payment_intent: string}}){
+
+    const paymentIntent = await stripe.paymentIntents.retrieve(
+        searchParams.payment_intent
+    )
+    if (paymentIntent.metadata.productId === null) return notFound()
+
+    const product = await db.product.findUnique({
+        where: { id: paymentIntent.metadata.productId},
+    })
+    if (product == null) return notFound()
+
+    const isSuccess = paymentIntent.status === "Succeeded"
+
     return (
         <div className="max-w-5xl w-full mx-auto space-y-8">
         <div className="flex gap-4 items-center">
