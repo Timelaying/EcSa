948c8befab910038a392828405d3ac5e61254b9d
Timelaying <makanjuola.timi@gmail.com>
2024-09-23 21:02:29 +0100

    stripe sucess page build up

--- ecsa/src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ ecsa/src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,3 +1,18 @@
 export default function SuccessPage(){
-    return <h1>Sucess</h1>
+    return (
+        <div className="max-w-5xl w-full mx-auto space-y-8">
+        <div className="flex gap-4 items-center">
+            <div className="aspect-video flex-shrink-0 w-1/3 relative">
+                <Image src={product.imagePath} fill alt={product.name} className="object-cover" />
+            </div>
+            <div>
+                <div className="text-lg">
+                    {formatCurrency(product.priceInCents / 100)}
+                </div>
+                <h1 className="text-2x1 font-bold">{product.name}</h1>
+                <div className="line-clamp-3 text-muted-foreground">{product.description}</div>
+            </div>
+        </div>
+    </div>
+    )
 }
