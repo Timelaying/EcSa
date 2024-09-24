f158ce12dbd0b9381fe9f4589f0a8b6bdefc63cd
Timelaying <makanjuola.timi@gmail.com>
2024-09-24 17:38:20 +0100

    ExpiredLink page

--- ecsa/src/app/(customerFacing)/products/download/[downloadVerificationId/route.ts
+++ ecsa/src/app/(customerFacing)/products/download/[downloadVerificationId/route.ts
@@ -1,5 +1,6 @@
 import db from "@/db/db";
 import { NextRequest, NextResponse } from "next/server";
+import fs from "fs/promises"
 
 export async function GET(
     req: NextRequest, {
@@ -14,4 +15,16 @@ export async function GET(
     if (data == null) {
         return NextResponse.redirect(new URL("/products/download/expired", req.url))
     }
+
+    const {size} = await fs.stat(data.product.filePath)
+    const file = await fs.readFile(data.product.filePath)
+    const extension = data.product.filePath.split(".").pop()
+
+    return new NextResponse(file,{
+        headers: {
+        "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+        "Content-Lenght": size.toString(),
+         },
+    })
+
 }
