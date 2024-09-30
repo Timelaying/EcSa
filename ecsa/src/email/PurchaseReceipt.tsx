df9eb6a15ef18568bde441712fc665566a458ab7
Timelaying <makanjuola.timi@gmail.com>
2024-09-30 10:46:24 +0100

    PurchaseReceipt

--- ecsa/src/email/PurchaseReceipt.tsx
+++ ecsa/src/email/PurchaseReceipt.tsx
@@ -1,9 +1,18 @@
-import { Html } from "@react-email/components";
+import { Body, Container, Head, Heading, Html, Preview, Tailwind } from "@react-email/components";
 
 export default function PurchaseReceiptEmail({ product }){
 return (
     <Html>
-        <Preview
+        <Preview>Download {product.name} and view receipt</Preview>
+        <Tailwind>
+            <Head />
+            <Body className="font-sans bg-white">
+                <Container className="max-w-xl">
+                    <Heading> Purchase Receipt</Heading>
+                    <OrderInformation />
+                </Container>
+            </Body>
+        </Tailwind>
     </Html>
 )
 }
