507075ce23230e39d570a73ded67623f333b5a3b
Timelaying <makanjuola.timi@gmail.com>
2024-10-01 09:10:40 +0100

    orer at and purchased on

--- ecsa/src/email/components/OrderInformation.tsx
+++ ecsa/src/email/components/OrderInformation.tsx
@@ -6,11 +6,23 @@ type OrderInformationProps = {
     downloadVerificationId: string
 }
 
+const dateFormater = new Intl.DateTimeFormat("en", { dateStyle: "medium"})
+
 export function OrderInformation({ order, product, downloadVerificationId}: OrderInformationProps){
     return (
         <>
         <Section>
             <Row>
+                <Column>
+                    <Text className="mb-0 text-grey-500 whitespace-nowrap text-nowrap mr-4">
+                        Order ID</Text>
+                    <Text className="mt-0 mr-4">{order.id}</Text>
+                </Column>
+                <Column>
+                    <Text className="mb-0 text-grey-500 whitespace-nowrap text-nowrap mr-4">
+                        Order ID</Text>
+                    <Text className="mt-0 mr-4">{order.id}</Text>
+                </Column>
                 <Column>
                     <Text className="mb-0 text-grey-500 whitespace-nowrap text-nowrap mr-4">
                         Order ID</Text>
