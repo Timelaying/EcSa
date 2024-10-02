8d1352e772a61fd3f877b9f969f86cda251f0c54
Timelaying <makanjuola.timi@gmail.com>
2024-10-02 10:17:20 +0100

    Submit button

--- ecsa/src/app/(customerFacing)/orders/page.tsx
+++ ecsa/src/app/(customerFacing)/orders/page.tsx
@@ -1,6 +1,9 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+"use cleint"
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useFormState } from "react-dom";
 
 export default function MyOrdersPage(){
     return <form className="max-2-xl mx-auto">
@@ -14,9 +17,16 @@ export default function MyOrdersPage(){
             <CardContent>
                 <div className="space-y-2">
                     <Label htmlFor="email">Email</Label>
-                    <Input type="email" required name="email" id="email"></Input>
+                    <Input type="email" required name="email" id="email" />
                 </div>
             </CardContent>
+            <CardFooter>
+                <SubmitButton />
+            </CardFooter>
         </Card>
     </form>
+}
+
+function SubmitButton(){
+    const {pending} = useFormState()
 }
