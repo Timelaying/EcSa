760a2e942d9b78704473631b5df31085b0fe44af
Timelaying <makanjuola.timi@gmail.com>
2024-10-02 10:42:31 +0100

    email orders cehck

---
+++ ecsa/src/actions/orders.tsx
@@ -0,0 +1,15 @@
+"use server"
+
+import { z } from "zod";
+
+const emailSchema = z.string().email()
+
+export async function emailOrderHistory(prevState: unknown, formData: FormData)
+: 
+Promise<{ message?: string; error?: string}> {
+    const result = emailSchema.safeParse(formData.get("email"))
+
+    if (result.success === false){
+        return {error: "Invalid email address"}
+    }
+}
