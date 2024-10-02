a946cae7ae92504b10964c088c03e1a2a9eb8bb7
Timelaying <makanjuola.timi@gmail.com>
2024-10-02 11:15:31 +0100

    ordes description

--- ecsa/src/email/OrderHistory.tsx
+++ ecsa/src/email/OrderHistory.tsx
@@ -1,36 +1,81 @@
-import { Body, Container, Head, Heading, Html, Preview, Tailwind } from "@react-email/components";
-import { OrderInformation } from "./components/OrderInformation";
-
-type OrderHistoryEmailProps = {
-    product: {
+import {
+    Body,
+    Container,
+    Head,
+    Heading,
+    Hr,
+    Html,
+    Preview,
+    Tailwind,
+  } from "@react-email/components"
+  import { OrderInformation } from "./components/OrderInformation"
+  import React from "react"
+  
+  type OrderHistoryEmailProps = {
+    orders: {
+      id: string
+      pricePaidInCents: number
+      createdAt: Date
+      downloadVerificationId: string
+      product: {
         name: string
         imagePath: string
         description: string
-    }
-
-    order: {id: string; createdAt: Date; pricePaidInCents: number}
-    downloadVerificationId: string
-}
-
-OrderHistoryEmail.PreviewProps = {
-    product: {name: "Product name", description: "string", imagePath: ""},
-    order: {id: crypto.randomUUID(), createdAt: new Date(), pricePaidInCents: 1000, },
-    downloadVerificationId: crypto.randomUUID()
-} satisfies OrderHistoryEmailProps
-
-export default function OrderHistoryEmail({ product,order, downloadVerificationId,} : OrderHistoryEmailProps){
-return (
-    <Html>
-        <Preview>Download {product.name} and view receipt</Preview>
+      }
+    }[]
+  }
+  
+  OrderHistoryEmail.PreviewProps = {
+    orders: [
+      {
+        id: crypto.randomUUID(),
+        createdAt: new Date(),
+        pricePaidInCents: 10000,
+        downloadVerificationId: crypto.randomUUID(),
+        product: {
+          name: "Product name",
+          description: "Some description",
+          imagePath:
+            "/products/5aba7442-e4a5-4d2e-bfa7-5bd358cdad64-02 - What Is Next.js.jpg",
+        },
+      },
+      {
+        id: crypto.randomUUID(),
+        createdAt: new Date(),
+        pricePaidInCents: 2000,
+        downloadVerificationId: crypto.randomUUID(),
+        product: {
+          name: "Product name 2",
+          description: "Some other desc",
+          imagePath:
+            "/products/db3035a5-e762-41b0-996f-d54ec730bc9c-01 - Course Introduction.jpg",
+        },
+      },
+    ],
+  } satisfies OrderHistoryEmailProps
+  
+  export default function OrderHistoryEmail({ orders }: OrderHistoryEmailProps) {
+    return (
+      <Html>
+        <Preview>Order History & Downloads</Preview>
         <Tailwind>
-            <Head />
-            <Body className="font-sans bg-white">
-                <Container className="max-w-xl">
-                    <Heading> Purchase Receipt</Heading>
-                    <OrderInformation order={order} product={product} downloadVerificationId={downloadVerificationId} />
-                </Container>
-            </Body>
+          <Head />
+          <Body className="font-sans bg-white">
+            <Container className="max-w-xl">
+              <Heading>Order History</Heading>
+              {orders.map((order, index) => (
+                <React.Fragment key={order.id}>
+                  <OrderInformation
+                    order={order}
+                    product={order.product}
+                    downloadVerificationId={order.downloadVerificationId}
+                  />
+                  {index < orders.length - 1 && <Hr />}
+                </React.Fragment>
+              ))}
+            </Container>
+          </Body>
         </Tailwind>
-    </Html>
-)
-}
+      </Html>
+    )
+  }
