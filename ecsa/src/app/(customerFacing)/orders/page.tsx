2411b8a8c6596240216af1021c3f11ed22662cbd
Timelaying <makanjuola.timi@gmail.com>
2024-10-02 10:14:07 +0100

    email input

--- ecsa/src/app/(customerFacing)/orders/page.tsx
+++ ecsa/src/app/(customerFacing)/orders/page.tsx
@@ -1,4 +1,6 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function MyOrdersPage(){
     return <form className="max-2-xl mx-auto">
@@ -9,7 +11,12 @@ export default function MyOrdersPage(){
                     Enter your email and we will email you your order history and download links
                 </CardDescription>
             </CardHeader>
-            
+            <CardContent>
+                <div className="space-y-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input type="email" required name="email" id="email"></Input>
+                </div>
+            </CardContent>
         </Card>
     </form>
 }
